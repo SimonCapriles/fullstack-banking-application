7ff009eab69864e18048b511f516fdb3513cbd0a
Simon Capriles <simon.capriles@kyohei.com.bo>
2022-02-03 09:38:07 -0400

    Added logout button and improved dynamic menu

--- src/navbar.js
+++ src/navbar.js
@@ -4,36 +4,47 @@ import {UserContext} from "./App";
 
 // Context step 5: Consume Context
 function NavBar() {
-    const {user} = React.useContext(UserContext);
+    const {user, setUser} = React.useContext(UserContext);
+    const logout = () => {setUser(false)}
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <Link className="navbar-brand m-2" data-toggle="tooltip" data-placement="bottom" title="Go to the home page" to="/">BadBank</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"/>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="If you don't have and account, create it!" to="/CreateAccount/">Create Account</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Have an account? Just login" to="/login/">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Excess money? Put it in!" to="/deposit/">Deposit</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Need money? Take only what you need" to="/withdraw/">Withdraw</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Shh...! don't tell anyone" to="/alldata/">AllData</Link>
-                    </li>
-                </ul>
-            </div>
+            { !user &&
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="If you don't have and account, create it!" to="/CreateAccount/">Create Account</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Have an account? Just login" to="/login/">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            }
+            { user &&
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Excess money? Put it in!" to="/deposit/">Deposit</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Need money? Take only what you need" to="/withdraw/">Withdraw</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" data-toggle="tooltip" data-placement="bottom" title="Shh...! don't tell anyone" to="/alldata/">AllData</Link>
+                        </li>
+                    </ul>
+                </div>
+            }
+
             { user &&
             <div className="text-light m-2">
                 Username: {user.name}<br/>
-                Balance: {user.balance}
+                Balance: {user.balance}<br/>
+                <Link to="/"><button onClick={logout}>Logout</button></Link>
             </div>
             }
         </nav>
