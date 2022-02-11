fbd20ad7281730603be0844cadfafefebbf3a6dc
Simon Capriles <simon.capriles@kyohei.com.bo>
2022-02-11 19:45:35 -0400

    Adding account removal function

--- src/account.js
+++ src/account.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-function Account({account}){
+function Account({account, remove}){
     return (
         <>
             <tr>
                 <td>{account.email}</td>
                 <td>{account.name}</td>
                 <td>{account.password}</td>
+                <td><button className="btn-danger" onClick={function(){remove(account.uid)}}>Delete</button></td>
             </tr>
+
         </>
     )
 }
--- src/alldata.js
+++ src/alldata.js
@@ -2,16 +2,41 @@ import React, {useEffect} from 'react';
 import Account from './account';
 import {serverUrl} from "./context";
 
-function AllData(){
+function AllData({firebase}){
     const [data, setData] = React.useState('');
     let users = null
     useEffect(()=>{
-        fetch(`${serverUrl}/account/all`)
-            .then(response => response.json())
-            .then(data => {setData(JSON.stringify(data));})
+        if (firebase.auth().currentUser) {
+            firebase.auth().currentUser.getIdToken()
+                .then(idToken => {
+                    fetch(`${serverUrl}/account/all`, {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': idToken
+                        }
+                    })
+                        .then(response => response.json())
+                        .then(data => {setData(JSON.stringify(data));})
+                })
+        }
+
     })
     if (data) {
         users = JSON.parse(data)
+    }
+
+    const removeUser = uid => {
+        console.log(uid);
+        (async () => {
+            var res = await fetch(`${serverUrl}/account/delete/${uid}`);
+            var data = await res.json();
+            if (data) {
+                alert('User removed')
+                setData(JSON.stringify(data));
+            } else {
+                alert('No data found')
+            }
+        })();
     }
 
     return (
@@ -26,7 +51,7 @@ function AllData(){
             { users &&
             <tbody>
                 {users.map((account, i) => (
-                    <Account key={i} index={i} account={account}/>
+                    <Account key={i} index={i} account={account} remove={removeUser}/>
                 ))}
             </tbody>
             }
